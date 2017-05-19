ddfce261a74fcfb1d5a84264724a4d6a00a4c923
Francagir <francagir@campusaula.com>
2017-05-19 10:46:04 +0200

    JUnit
    Realización del test JUnit

--- EDE/JUnit/Actividad-7/src/com/practicas/ede/BinString.java
+++ EDE/JUnit/Actividad-7/src/com/practicas/ede/BinString.java
@@ -2,40 +2,47 @@
 package com.practicas.ede;
 
 // ¿Qué puede hacer esta Clase?
+//Convertir  un numero y una cadena a binario
 public class BinString {
-	public BinString(){}
+	public BinString() {
+	}
 
 	// ¿Qué hace este método?
-	public String convertir(String s){
+	// convierte a binario el resultado de la suma
+	public String convertir(String s) {
 		return aBinario(sumar(s));
 	}
-	
+
 	// ¿Qué hace este método?
-	public int sumar(String s){
+	// Suma los caracteres de la cadena (en valor binario)
+	public int sumar(String s) {
 		String s1 = s.substring(0);
 		int sum = 0;
-		while(!s1.equals(""))
-		{
-			sum = sum+(int)(s1.charAt(0));
+		while (!s1.equals("")) {
+			sum = sum + (int) (s1.charAt(0));
 			s1 = s1.substring(1);
 		}
-		
+
 		return sum;
 	}
-	
+
 	// ¿Qué hace este método?
-	public String aBinario(int n){
-	    String s = "";
-	    
-	    while (n > 0)
-	    {
-	    	if(n%2==0)
-	    		s = "0"+s;
-	    	else
-	    		s = "1"+s;
-	        n = n / 2;
-	    }
-	    
-	    return s;
+	//Convierte un numero a binario
+	public String aBinario(int n) {
+		String s = "";
+
+		while (n > 0) {
+			if (n % 2 == 0)
+				s = "0" + s;
+			else
+				s = "1" + s;
+			n = n / 2;
+		}
+
+		return s;
+	}
+
+	public static void main(String[] args) {
+
 	}
 }
--- EDE/JUnit/Actividad-7/src/com/practicas/ede/BinStringTest.java
+++ EDE/JUnit/Actividad-7/src/com/practicas/ede/BinStringTest.java
@@ -11,26 +11,33 @@ import org.junit.Test;
 public class BinStringTest {
 
 	private BinString binString;
-	
-	public BinStringTest(){
-		// Completar
-	}
-	
-	@Test
-	public void  sumarTest(){
-		// Test de la función sumar
 
+	public BinStringTest() {
+		binString = new BinString();
 	}
-	
+
+	@Test
+	public void sumarTest() {
+		// Test de la función sumar
+		int expected = 0;
+		assertEquals(expected, binString.sumar(""));
+		expected = 292;
+		assertEquals(expected, binString.sumar("aba"));
+		expected = 292;
+	}
+
 	@Test
 	public void aBinarioTest() {
 		// Test de la función aBinario
+		String expected = "100100100";
+		assertEquals(expected, binString.aBinario(292));
 
 	}
-	
+
 	@Test
 	public void convertirTest() {
-		// Test de la función convertirTest
 
+		String expected = "100100100";
+		assertEquals(expected, binString.convertir("aba"));
 	}
 }
